595b63b6ca603b5be07ee89abad7801e5ee92b96
macskeptic <macskeptic@gmail.com>
2010-01-03 18:52:13 -0200

    added a failing test

--- specs/project_spec.rb
+++ specs/project_spec.rb
@@ -21,5 +21,31 @@ describe Project do
     end
     
     it "should provide a list of every single file that will be packaged for delivery" do
+        temp = TempHelper::TempFolder
+        TempHelper::touch '', 'myProject.dll'
+        TempHelper::touch '', 'myProject.pdb'
+        TempHelper::touch '', 'myProject.foo'
+        TempHelper::touch '', 'maiProject.dll'
+        TempHelper::touch '', 'maiProject.pdb'
+        TempHelper::touch '', 'maiProject.foo'
+        TempHelper::touch '', 'maiProject.exe'
+        TempHelper::touch '', 'maiProjecto.exe'
+    
+        project = Project.new :name => "MyProject"
+        
+        project.should_receive(:path_to_binaries).with('release').and_return(temp)
+        
+        files_to_deliver = project.files_to_deliver
+        
+        files_to_deliver.should_not be_nil
+        files_to_deliver.should_not be_empty
+        files_to_deliver.should(have temp + '/myProject.dll')
+        files_to_deliver.should(have temp + '/maiProject.dll')
+        files_to_deliver.should(have temp + '/maiProject.exe')
+        files_to_deliver.should(have temp + '/maiProjecto.exe')
+        files_to_deliver.should_not(have temp + '/myProject.pdb')
+        files_to_deliver.should_not(have temp + '/myProject.foo')
+        files_to_deliver.should_not(have temp + '/maiProject.pdb')
+        files_to_deliver.should_not(have temp + '/maiProject.foo')
     end
 end
