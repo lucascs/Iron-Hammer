bd3f03b4cf91a26fce8d65866afb5a6879bc1fa8
macskeptic <macskeptic@gmail.com>
2010-01-03 18:30:02 -0200

    added temphelper

--- helpers/spec_helper.rb
+++ helpers/spec_helper.rb
@@ -1,3 +1,4 @@
+Dir[File.dirname(__FILE__) + '/*.rb'].each { |file| require file.sub('.rb', '') unless file == 'spec_helper.rb' }
 Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file.sub('.rb', '') }
 
 require 'spec'
---
+++ helpers/temp_helper.rb
@@ -0,0 +1,24 @@
+module TempHelper
+    TempFolder = File.dirname(__FILE__) + '/../temp/'
+    
+    def self.cleanup
+        FileUtils.rm_rf TempFolder
+        md ''
+    end
+
+    def self.md path
+        FileUtils.mkpath path.inside_temp_dir
+    end
+    
+    def self.touch path, file
+        md path
+        FileUtils.touch file.inside_temp_dir
+    end
+    
+end unless defined? TempHelper
+
+class String
+    def inside_temp_dir
+        TempHelper::TempFolder + self
+    end
+end
