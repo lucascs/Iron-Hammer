2713b63ddef2b58d22b946d7240780176b045fa7
macskeptic <macskeptic@gmail.com>
2010-01-09 23:15:55 -0200

    changed thefiler implementation

--- lib/the_filer.rb
+++ lib/the_filer.rb
@@ -1,7 +1,6 @@
 module TheFiler
   def self.copy source, destination
-    (destination_path_array = destination.split('/')).pop
-    destination_path = File.join *destination_path_array
+    destination_path = File.dirname destination
     FileUtils.mkpath destination_path unless destination_path.empty? || File.exists?(destination_path)
     FileUtils.cp source, destination
   end
