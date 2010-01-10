74d209253997063cae3dc095e8d69dc08ebc593e
macskeptic <macskeptic@gmail.com>
2010-01-10 17:11:55 -0200

    added specs for thefiler

--- lib/the_filer.rb
+++ lib/the_filer.rb
@@ -1,7 +1,17 @@
 module TheFiler
-  def self.copy! source, destination
+  def self.copy_file! source, destination
+    raise(ArgumentError.new 'source must be a file') if File.directory?(source)
     TheFiler::create_path! File.dirname(destination)
     FileUtils.cp source, destination
+  end
+  
+  def self.copy_directory! source, destination
+    raise(ArgumentError.new 'source must be a directory') unless File.directory?(source)
+    FileUtils.cp_r source, destination
+  end
+  
+  def self.copy! source, destination
+    File.directory?(source) ? TheFiler::copy_directory!(source, destination) : TheFiler::copy_file!(source, destination)
   end
   
   def self.write! params={}
@@ -9,7 +19,7 @@ module TheFiler
     File.open(File.join(path, name = params[:name]), 'w') { |file| file.write(content = params[:content]) }
   end
   
-  def self.read *path
+  def self.read_file *path
     File.open(File.join(*path), 'r') { |file| file.read } unless path.nil? || path.empty?
   end
   
