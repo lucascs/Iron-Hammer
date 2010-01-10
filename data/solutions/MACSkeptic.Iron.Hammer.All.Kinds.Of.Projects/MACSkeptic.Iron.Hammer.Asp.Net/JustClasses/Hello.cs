051b185f0f6b2a2f06b820b13b4d2f20a7e253fc
macskeptic <macskeptic@gmail.com>
2010-01-10 15:47:27 -0200

    added a solution with many sorts of projects to the data folder, for testing purposes

---
+++ data/solutions/MACSkeptic.Iron.Hammer.All.Kinds.Of.Projects/MACSkeptic.Iron.Hammer.Asp.Net/JustClasses/Hello.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace MACSkeptic.Iron.Hammer.Asp.Net.JustClasses
+{
+    public class Hello
+    {
+    }
+}
