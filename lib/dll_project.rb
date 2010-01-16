34a916622c5bc825ec48d319e1aea30847577195
macskeptic <macskeptic@gmail.com>
2010-01-16 00:14:32 -0200

    removed unused code

--- lib/dll_project.rb
+++ lib/dll_project.rb
@@ -65,40 +65,10 @@ class DllProject < Project
   end
   
   def configuration params={}
-    conf_paths = paths_to_configuration(params)
-    conf = []
-    conf_paths.each do |path|
-      Dir[File.join(path, prioritary_configuration(params))].each do |file|
-        deliverable = Deliverable.new(
-          :path_on_package => '', 
-          :actual_path => path, 
-          :actual_name => file.split('/').last,
-          :name_on_package => file.split('/').last.sub('.' + environment(params), '')
-        )
-        conf << deliverable unless conf.find { |d| d.name_on_package == deliverable.name_on_package }
-      end
     
-      Dir[File.join(path, CONFIGURATION)].each do |file|
-        deliverable = Deliverable.new(
-          :path_on_package => '', 
-          :actual_path => path, 
-          :actual_name => file.split('/').last
-        )
-        conf << deliverable unless conf.find { |d| d.name_on_package == deliverable.name_on_package }
-      end
-    end
-    conf
-  end
-  
-  def prioritary_configuration params={}
-    CONFIGURATION + environment(params)
   end
   
   def path_to_binaries params={}
     File.join(@path, 'bin', run_configuration(params))
-  end
-  
-  def paths_to_configuration params={}
-    [@path, path_to_binaries(params)]
   end
 end unless defined? DllProject
