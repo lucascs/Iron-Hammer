d183b6af6c533af2bec4006dcdab56fe6fc3e593
unknown <lucas@.fabrica.locaweb.com.br>
2010-01-22 15:53:17 -0200

    creating rake tasks for building and testing

---
+++ lib/iron_hammer/tasks.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'rake/clean'
+require 'iron_hammer'
+
+CLEAN.include("TestResults/**")
+
+namespace :iron do
+    task :initialize do
+	    @anvil = Anvil.load_solution_from '.'
+	    @anvil.load_projects_from_solution
+	    @hammer = Hammer.new :visual_studio_path => VISUAL_STUDIO_PATH
+	    FileUtils.mkdir 'TestResults' unless (File.exists?('TestResults') && File.directory?('TestResults'))
+    end
+
+    desc 'Builds the solution'
+    task :build => [:clean, :initialize] do
+	    sh @hammer.build @anvil.solution
+    end
+
+    desc 'Runs the unit tests'
+    task :unit => [:build] do
+	    sh @hammer.test *@anvil.test_projects
+    end
+end
+
