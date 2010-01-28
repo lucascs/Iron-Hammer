2ff5717106ace5588fe7860136669230089607e4
Alessandro <alessandro.santos@locaweb.com.br>
2010-01-28 14:59:31 -0200

    starting ivy builder

--- spec/iron_hammer/utils/ivy_builder_spec.rb
+++ spec/iron_hammer/utils/ivy_builder_spec.rb
@@ -8,11 +8,16 @@ module IronHammer
         
         @ivy = IvyBuilder.new project
         
-        @ivy.to_s.strip.should == "
-<ivy-module version=\"2.0\">
-  <info organisation=\"MyProject\" module=\"MyProject\"/>
-</ivy-module>
-        ".strip
+        @ivy.to_s.strip.should be_equal_xml "
+              <ivy-module version=\"2.0\">
+                <info organisation=\"MyProject\" module=\"MyProject\"/>
+              </ivy-module>"
+      end
+    end
+    
+    def be_equal_xml xml
+      simple_matcher("a xml like #{xml}") do |other|
+        other.gsub(/\s+/, ' ') == xml.gsub(/\s+/, ' ')
       end
     end
   end
