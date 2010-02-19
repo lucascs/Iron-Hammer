ea2b487e3a6aef3bf8ee56da0ee7a15e70cd5202
lucascs <lucasmrtuner@gmail.com>
2010-02-19 12:21:08 -0200

    refactoring tasks

--- lib/iron_hammer/tasks.rb
+++ lib/iron_hammer/tasks.rb
@@ -12,10 +12,15 @@ require 'iron_hammer/tasks/test'
 namespace :iron do
 
     @anvil = Anvil.load_from(ENV['SolutionDir'] || '.')
-    @hammer = Hammer.new(
-      (defined?(VISUAL_STUDIO_PATH) ? {:visual_studio_path => VISUAL_STUDIO_PATH} : {}).merge(
-      :configuration => ENV['Configuration']
-      ))
+
+    @options = {}
+    @options[:visual_studio_path] = VISUAL_STUDIO_PATH  if defined?(VISUAL_STUDIO_PATH)
+    @options[:dot_net_path]       = DOT_NET_PATH        if defined?(DOT_NET_PATH)
+    @options[:fxcop_path]         = FXCOP_PATH          if defined?(FXCOP_PATH)
+    @options[:configuration]      = ENV['Configuration']
+
+    @hammer = Hammer.new @options
+
     FileUtils.mkdir 'TestResults' unless (File.exists?('TestResults') && File.directory?('TestResults'))
 
     desc 'Executes the default lifecycle'
